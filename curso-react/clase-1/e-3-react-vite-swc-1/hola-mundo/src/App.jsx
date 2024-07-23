31df67b1feb9a62813b508bae911c5cecc1d9342
Yon Cayllahua <yoncayllahua092150@gmail.com>
2024-07-22 23:49:36 -0500

    Clase 1 y ejemplo 0 culminado

--- curso-react/clase-1/e-3-react-vite-swc-1/hola-mundo/src/App.jsx
+++ curso-react/clase-1/e-3-react-vite-swc-1/hola-mundo/src/App.jsx
@@ -1,10 +1,99 @@
+import { useState } from "react"
 import "./App.css"
 import { TwitterFollowCard } from "./TwitterFollowCard"
 
 export function App() {
-    return(
+    const [name, setName] = useState('cayllahua-yon')  // ESTO ES MALA PRACTICA-- YA QUE EL ESTADO INICIAL SE INICIALIZA SOLO UNA VEZ
+    // const formatForUserName = (userName) => `@${userName}`; // esta funcion no se esta ejecutando aqui se esta pasando como tal...
+    // const formatterUserName = (<span>@{userName}</span>); 
+    // const formatterUserName = <span>@cayllahua-yon</span>; // lo que pasamos es un ELEMENTO
+    // const cayllahua = {isFollowing: true, userName: 'cayllahua-yon'}
+    // const luz = {isFollowing: true, userName: 'luz'}
+
+    console.log('render with name:' , name)
+
+    const [isFollowing, setIsFollowing] = useState(false)  // ESTO ES MALA PRACTICAS
+
+    const users = [
+        {
+            userName: 'cayllahua-yon',
+            name: 'Yon Cayllahua',
+            isFollowing: true
+        },
+        {
+            userName: 'luz',
+            name: 'Luz Cayllahua',
+            isFollowing: true
+        },
+        {
+            userName: 'Eli',
+            name: 'Eli Palomino',
+            isFollowing: true
+        },
+        {
+            userName: 'yon',
+            name: 'Yon Utani',
+            isFollowing: true
+        },
+    ]
+
+    return(// NOta:: tambien podemos pasar como prom funciones
         // <article style={{display: 'flex', alignItems: 'center', color: '#fff'}}>
-        <TwitterFollowCard />        
+        <section className="App">
+
+            {
+                users.map((user)=>{
+                    const {userName, name, isFollowing} = user;
+                    return (
+                        <TwitterFollowCard key={userName} userName={userName} initialIsFollowing={isFollowing} >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+
+            {/* <TwitterFollowCard formatterUserName={formatterUserName}  isFollowing={true} userName="cayllahua-yon" name="Yon Cayllahua Utani"   />         */}
+            {/* <TwitterFollowCard number={[1,2,3]}  isFollowing={true} userName="cayllahua-yon" name="Yon Cayllahua Utani"   />         */}
+            
+            {/* <TwitterFollowCard  isFollowing={true} userName="cayllahua-yon" name="Yon Cayllahua Utani"   />        
+            <TwitterFollowCard  isFollowing={false} userName="caylahua" name="Yon Cayllahua Utani"   />        
+            <TwitterFollowCard  isFollowing userName="manuel" name="Manuel Utani"   />        
+            <TwitterFollowCard  isFollowing userName="pedro" name="Pedro Utani"   />         */}
+            
+            {/* <TwitterFollowCard  isFollowing userName="cayllahua-yon" > 
+                Yon Cayllahua Utani
+            </ TwitterFollowCard>  */}
+           
+            
+            {/* <TwitterFollowCard  userName="Luz" > 
+                Luz
+            </ TwitterFollowCard>  */}
+
+            {/* <TwitterFollowCard  {...cayllahua} > 
+                Yon Cayllahua Utani
+            </ TwitterFollowCard> 
+            
+            <TwitterFollowCard   {...luz} > 
+                Luz
+            </ TwitterFollowCard>  */}
+            
+            {/* <TwitterFollowCard   userName={name} initialIsFollowing={isFollowing} > 
+                Yon Cayllahua Utani
+            </ TwitterFollowCard> 
+
+            <TwitterFollowCard   userName="luz" initialIsFollowing={isFollowing} > 
+                Luz
+            </ TwitterFollowCard>  */}
+
+            
+
+            <button onClick={()=>{setName('palomino')}}>
+                Cambiar Nombre de {name}
+            </button>
+            <button onClick={()=>{setIsFollowing(!isFollowing)}}>
+                Cambiar App 
+            </button>
+        </section>
     )
 
-}
+} 
--- curso-react/clase-1/e-3-react-vite-swc-1/hola-mundo/src/TwitterFollowCard.jsx
+++ curso-react/clase-1/e-3-react-vite-swc-1/hola-mundo/src/TwitterFollowCard.jsx
@@ -1,17 +1,44 @@
-export function TwitterFollowCard({useName, name, isFollowing}) {
+import { useState } from "react";
+
+export function TwitterFollowCard({children, userName='unknown', name, initialIsFollowing}) {
+    // const imgSrc =  `https://unavatar.io/${userName}`;
+    // console.log(isFollowing);
+    // const addAt = (userName) => `@${userName}`; // en vez de tener una funcion dentro, se podria pasar desde fuera.
+    // const [isFollowing, setIsFollowing] = useState(false);
+    console.log(initialIsFollowing)
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+    const text = isFollowing ? 'Siguiendo':'Seguir';
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
+    console.log('Render with userName ', userName )
+
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
-                <img className="tw-followCard-avatar" src="https://avatars.githubusercontent.com/u/64764017?v=4" />
+                <img alt="el avatar de cayllahua yon" className="tw-followCard-avatar" src={`https://unavatar.io/${userName}`} />
                 <div className="tw-followCard-info">
-                    <strong> Yon Cayllahua Utani</strong>
-                    <span className="tw-followCard-infoUserName">@{useName}</span>
+                    {/* <strong>{name}</strong> */}
+                    <strong>{children}</strong>
+                    <span className="tw-followCard-infoUserName">@{userName}</span>
+                    {/* <span className="tw-followCard-infoUserName">{formatUserName(userName)}</span> */}
+                    {/* <span className="tw-followCard-infoUserName">{formatterUserName}</span> */}
+                    {/* <span className="tw-followCard-infoUserName">@{userName}</span> */}
+                    {/* <span className="tw-followCard-infoUserName">{addAt(userName)}</span> */}
                 </div>
             </header>
 
             <aside>
-                <button className="tw-followCard-button">
-                    Seguir
+                {/* <button className="tw-followCard-button"> */}
+                <button className={buttonClassName} onClick={handleClick} >
+                   {/* Seguir        <-- Children ?   */}
+                   
+                   <span className="tw-followCard-text">{text}</span>
+                   <span className="tw-followCard-stopFollow">Dejar de seguir</span>
                 </button>
             </aside>
         </article>
